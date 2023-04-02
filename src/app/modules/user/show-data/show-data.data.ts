54b9da208033310291b114bdfb9ee7eedf7341ff
tototonzq <117251703+tototonzq@users.noreply.github.com>
2023-04-02 23:27:07 +0700

    [ ADD ]

--- src/app/modules/user/show-data/show-data.data.ts
+++ src/app/modules/user/show-data/show-data.data.ts
@@ -1,5 +1,5 @@
 export const DATA_TABLE: any = [
-  // { name: 'XXXX', age: 'XXXX', city: 'XXXX' },
-  // { name: 'XXXX', age: 'XXXX', city: 'XXXX' },
-  // { name: 'XXXX', age: 'XXXX', city: 'XXXX' },
+  { name: 'XXXX', age: 'XXXX', city: 'XXXX' },
+  { name: 'XXXX', age: 'XXXX', city: 'XXXX' },
+  { name: 'XXXX', age: 'XXXX', city: 'XXXX' },
 ];
