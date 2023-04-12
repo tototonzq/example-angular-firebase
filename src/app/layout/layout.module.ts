622f5483b5ea623aec2bcde73bd5a66918b576fb
tototonzq <117251703+tototonzq@users.noreply.github.com>
2023-04-12 10:30:35 +0700

    [ ADD ]

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,10 @@
+// import { NgModule } from '@angular/core';
+
+// const modules = [];
+
+// @NgModule({
+//   declarations: [],
+//   imports: [],
+//   exports: [...modules],
+// })
+// export class LayoutModule {}
