4f071920a61465901754976e6e1cace4a4aea066
tototonzq <117251703+tototonzq@users.noreply.github.com>
2023-05-18 00:14:55 +0700

    [ ADD ]

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  MENU_DROPDOWN_GROUP_ADMIN,
+  MENU_DROPDOWN_MAJOR_ADMIN,
+  MENU_DROPDOWN_MANAGER_ADMIN,
+} from '../../admin/manager/manager.data';
+import Swal from 'sweetalert2';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DropdownRole } from '../../admin/manager/store/models/manager.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,7 +18,87 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
-  constructor() {}
+  /* -------------------------------------------------------------------------- */
+  //*                                 constructor                                */
+  /* -------------------------------------------------------------------------- */
+  constructor(
+    private _auth: AngularFireAuth,
+    private _firestore: AngularFirestore,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {}
 
+  /* -------------------------------------------------------------------------- */
+  //*                                  variables                                 */
+  /* -------------------------------------------------------------------------- */
+  form: FormGroup = new FormGroup({
+    user: new FormControl('', [Validators.required]),
+    role: new FormControl('student'),
+    username: new FormControl(''),
+    major: new FormControl('', [Validators.required]),
+    password: new FormControl('123456789'),
+    code: new FormControl('', [Validators.required]),
+    group: new FormControl('', [Validators.required]),
+  });
+
+  isLoadingAdd$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  /* -------------------------------------------------------------------------- */
+  //*                                 life circle                                */
+  /* -------------------------------------------------------------------------- */
   ngOnInit(): void {}
+  /* -------------------------------------------------------------------------- */
+  //*                                  functions                                 */
+  /* -------------------------------------------------------------------------- */
+
+  Add(item?: any): void {
+    this.isLoadingAdd$.next(true);
+    // TODO : Set Value
+    const data: any = {
+      role: this.form.value.role,
+      code: this.form.value.code,
+      username: this.form.value.code,
+      password: this.form.value.password,
+      user: this.form.value.user,
+      group: this.form.value.group,
+      major: this.form.value.major,
+    };
+    // console.log(this.form.value);
+
+    if (this.form.invalid) {
+      Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        title: `กรุณาใส่ข้อมูลให้ครบถ้วน`,
+        showConfirmButton: false,
+        timer: 1200,
+      });
+      return;
+    }
+    // //* Loading Add data
+    // else
+    this._firestore
+      .collection('users')
+      .doc(item)
+      .set(data)
+      .then((res) => {
+        this.isLoadingAdd$.next(false);
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: `เพิ่มข้อมูลสำเร็จ`,
+          showConfirmButton: false,
+          timer: 1200,
+        });
+        this._router.navigate(['/sign-in']);
+      });
+  }
+  /* -------------------------------------------------------------------------- */
+  //*                                    data                                    */
+  /* -------------------------------------------------------------------------- */
+  // TODO : get role
+  dropdown: DropdownRole[] = MENU_DROPDOWN_MANAGER_ADMIN;
+  // TODO : get group
+  dropdownGroup: any[] = MENU_DROPDOWN_GROUP_ADMIN;
+  // TODO : get major
+  dropdownMajor: any[] = MENU_DROPDOWN_MAJOR_ADMIN;
 }
--- src/app/modules/auth/sign-up/sign-up.module.ts
+++ src/app/modules/auth/sign-up/sign-up.module.ts
@@ -4,11 +4,19 @@ import { RouterModule } from '@angular/router';
 import { signUpRoutes } from './sign-up.routing';
 import { SignUpComponent } from './sign-up.component';
 import { SignUpListComponent } from './sign-up-list/sign-up-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoadersScreenModule } from 'src/app/shared/components/loaders-screen/loaders-screen.module';
 
 const components = [SignUpComponent, SignUpListComponent];
 
 @NgModule({
   declarations: [...components],
-  imports: [CommonModule, RouterModule.forChild(signUpRoutes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(signUpRoutes),
+    ReactiveFormsModule,
+    FormsModule,
+    LoadersScreenModule,
+  ],
 })
 export class SignUpModule {}
